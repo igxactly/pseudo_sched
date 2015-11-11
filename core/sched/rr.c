d3c5776a20d38082e6fc8680a31cfa444f351bf2
Ingu Kang <igxactly@gmail.com>
2015-11-11 17:48:57 +0900

    Defined struct scheduler and Round Robin datatypes

--- core/sched/rr.c
+++ core/sched/rr.c
@@ -1,2 +1,37 @@
 #include <lib/dlist.h>
 #include <scheduler_skeleton.h>
+
+/* TODO: Declare array or some variables to store information
+ * e.g. Global_array named rq_entry_pool_rr
+ */
+
+typedef enum state_rr {
+    DETACHED,
+    WAITING,
+    RUNNING
+} state_rr;
+
+struct rq_entry_rr {
+    struct list_head head;
+    
+    /* TODO: set field types to abstract types */
+    int vcpuid; //vcpuid_t
+    int tick; //tick_t
+    int tick_reset_val; //tick_t
+    enum state_rr state;
+};
+
+/* Function definitions goes here */
+
+
+
+
+/* TODO: assign proper function's address to sched-algo struct */
+const struct scheduler sched_rr ={
+    .init = NULL,
+    .vcpu_register = NULL,
+    .vcpu_unregister = NULL,
+    .vcpu_attatch = NULL,
+    .vcpu_detach = NULL,
+    .do_scheduler = NULL
+};
--- include/scheduler_skeleton.h
+++ include/scheduler_skeleton.h
@@ -1,7 +1,18 @@
 #ifndef _SCHEDULER_SKELETON_H_
 #define _SCHEDULER_SKELETON_H_
 
+#include <stddef.h>
 
+struct scheduler {
+    unsigned int sched_id;
+    
+    int (*init)(void);
+    int (*vcpu_register)(void);
+    int (*vcpu_unregister)(void);
+    int (*vcpu_attatch)(void);
+    int (*vcpu_detach)(void);
+    int (*do_scheduler)(void);
+};
 
 #endif /* _SCHEDULER_SKELETON_H_ */
 
